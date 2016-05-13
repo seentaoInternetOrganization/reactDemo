567e6d680d33abf5281a6891bf6a9bcd63a0c3f1
AngusChen <angus1105@gmail.com>
2016-05-13 10:50:11 +0800

    可以运行

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        这是一个基于ReactJS Flux的demo
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import {Link} from 'react-router';
+// import HomeStore from '../stores/HomeStore'
+// import HomeActions from '../actions/HomeActions';
+// import {first, without, findWhere} from 'underscore';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = HomeStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    // HomeStore.listen(this.onChange);
+    // HomeActions.getTwoCharacters();
+  }
+
+  componentWillUnmount() {
+    // HomeStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        Demo from Home HAHAHAHAH
+      </div>
+    );
+  }
+}
+
+export default Home;
