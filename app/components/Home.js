5c61c7ed8b539dfe7e21d847f8ba9b3d991c49d1
AngusChen <angus1105@gmail.com>
2016-05-16 12:56:50 +0800

    http request demo

--- app/components/Home.js
+++ app/components/Home.js
@@ -30,25 +30,6 @@ class Home extends React.Component {
   }
 
   render() {
-
-    // var characterNodes = this.state.characters.map((character, index) => {
-    //   return (
-    //     <div key={character.characterId} className={index === 0 ? 'col-xs-6 col-sm-6 col-md-5 col-md-offset-1' : 'col-xs-6 col-sm-6 col-md-5'}>
-    //       <div className='thumbnail fadeInUp animated'>
-    //         <img onClick={this.handleClick.bind(this, character)} src={'http://image.eveonline.com/Character/' + character.characterId + '_512.jpg'}/>
-    //         <div className='caption text-center'>
-    //           <ul className='list-inline'>
-    //             <li><strong>Race:</strong> {character.race}</li>
-    //             <li><strong>Bloodline:</strong> {character.bloodline}</li>
-    //           </ul>
-    //           <h4>
-    //             <Link to={'/characters/' + character.characterId}><strong>{character.name}</strong></Link>
-    //           </h4>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   );
-    // });
     var loanNodes = this.state.loans.map((loanItem, index) => {
         return <div key = {loanItem.loanId} className = 'index_03_00'>
                   <div>{loanItem.loanName}</div>
