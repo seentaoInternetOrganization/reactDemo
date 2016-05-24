30d0a839b778ae012da9f35f2ffb9855a5e31a35
李庆楠 <389828004@qq.com>
2016-05-24 16:35:24 +0800

    选单结果，剩切换表 发请求

--- app/components/OrderResult.js
+++ app/components/OrderResult.js
@@ -16,6 +16,7 @@ class OrderResult extends React.Component {
     OrderResultStore.listen(this.onChange);
     //发送请求，参数：apiName,{}
     OrderResultActions.getOrderResult("/api/getOrderResult", {selectOrderStage:1,sSysId:"aaa"})
+    //radioBtn回调函数
     var token1 =  PubSub.subscribe( 'one', this.dealIndex );
   }
 
@@ -37,32 +38,20 @@ class OrderResult extends React.Component {
     var size = this.state.orderResults.length;
     var dqNodes = this.state.orderResults.map((item, index) => 
     {
-        // var array = item.orderResult.split(",");
-        // var teamInfo = array.map((arrayItem, index) => 
-        // {
-        //     var teamArray = arrayItem.split("_");
-        //     return  <div key={index}>
-        //               {teamArray[0]}
-        //               {teamArray[1]}
-        //             </div>
-        // });
+        var array = item.orderResult.split(",");
+        var teamInfo = array.map((arrayItem, index) => 
+        {
+            var teamArray = arrayItem.split("_");
+            return    <li><span>{teamArray[0]}</span>
+                          <i>获得数量：</i><em>{teamArray[1]}</em>
+                      </li>                      
+        });
         return  <div className="column common_label_content_00 common_orl_table" key={index}>
                     <h2>{"订单号:"+item.pOrderId}</h2>
                     <ul>
-                        <li><span>1.南华科技公司</span>
-                            <i>获得数量：</i><em>20</em>
-                        </li>
-                        <li><span>1.南华科技公司</span>
-                            <i>获得数量：</i><em>20</em>
-                        </li>
-                        <li><span>1.南华科技公司</span>
-                            <i>获得数量：</i><em>20</em>
-                        </li>
-                        <li><span>1.南华科技公司</span>
-                            <i>获得数量：</i><em>20</em>
-                        </li>
+                        {teamInfo}
                     </ul>
-                    <div className=""><span>我获得的数量：</span><i>{item.myOrderCount}</i></div>
+                    <div><span>我获得的数量：</span><i>{item.myOrderCount}</i></div>
                 </div>        
     });
 
